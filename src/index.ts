ccda934655605f00348d42e4a30bbabfcdb49c7d
Rafael Gonzaga <rafael_hgcarlos@hotmail.com>
2025-09-01 23:04:41 -0300

    feat: atualiza estrutura do index para hospedagem

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,20 @@ const FRONTEND_URL = "https://mybooknest.pages.dev";
 
 app.use(cors({
     origin: FRONTEND_URL,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true
+}));
+
+app.options("*", cors({
+    origin: FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
 }));
 
 app.use(express.json());
 app.use("/auth", authRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
