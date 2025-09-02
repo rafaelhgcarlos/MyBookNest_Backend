6dbd5c3f42fb0f2e183f2a183c1af027b05bdaf2
Rafael Gonzaga <rafael_hgcarlos@hotmail.com>
2025-09-01 23:34:53 -0300

    feat: atualiza estrutura do index para hospedagem

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,13 @@ app.use(cors({
     credentials: true
 }));
 
+app.options("*", cors({
+    origin: FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
+
 app.use(express.json());
 app.use("/auth", authRoutes);
 
